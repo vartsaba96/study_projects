50aa891f016a244654ca8a630b71a471407a4579
Pavlo <mcvarc@gmail.com>
2019-05-14 21:31:49 +0300

    Users api

--- src/component/Users/Users.jsx
+++ src/component/Users/Users.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './users.module.css';
-
-
-
+import Axios from 'axios';
+import userPhoto from '../../assets/images/ava.jpg';
 const Users = (props) => {
+   if (props.users.length===0){
+       Axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+           props.setUsers(response.data.items);
+       });
+   }
    return <div>
        {props.users.map(u=> <div key={u.id}>
         <span>
             <div>
-                <img src={u.photoUrl} className={styles.userPhoto} />
+                <img src={u.photos.small !=null?u.photos.small :userPhoto } className={styles.userPhoto} />
             </div>
             <div>
                 {u.folowwed ?
@@ -18,13 +22,13 @@ const Users = (props) => {
         </span>
         <span>
             <span>
-                <div>{u.fullname}</div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
             </span>
         </span>
            <span>
-               <div>{u.location.city}</div>
-               <div>{u.location.country}</div>
+               <div>{"u.location.city"}</div>
+               <div>{"u.location.country"}</div>
            </span>
        </div>)
        }
--- src/state/usersPageReducer.js
+++ src/state/usersPageReducer.js
@@ -3,16 +3,7 @@ const UNFOLOW = 'UNFOLOW';
 const SET_USERS='SET_USERS';
 
 let initialState = {
-   users: [
-       {id:1, photoUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCLaYWwpAG6Jpw9v1pEGn_l_EMK6MKUfq34dDFiRg45z_pb_1X',
-           folowwed:true, fullname:'Pavlo V', status:'Just happy', location: { country:'Ukraine', city:'Kyiv'}},
-       {id:2,
-           photoUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-TCGjx9inw7zgX08bGQhUmGoNT1U_LqdhRVmM5rtdcZLUTLkH5w',
-           folowwed:false,fullname:'Dima D', status:'Just happy for fun', location: { country:'Ukraine', city:'Kharkiv'}},
-       {id:3,
-           photoUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkcdejCAtD-QIefUKZpNEIWgg00oUDXbimTncg0dgpaE2SkLfD',
-           folowwed:false,fullname:'Vitalik S', status:'I am sad', location: { country:'Ukraine', city:'Donetsk'}}
-   ]
+   users: [ ]
 };
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
