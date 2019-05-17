4a4fcda0d656f068e250c624079b1bc7560e8614
Varts <mcvarc@gmail.com>
2019-05-18 00:10:53 +0300

    Pagination for Users Page

--- src/component/Users/Users.jsx
+++ src/component/Users/Users.jsx
@@ -8,16 +8,35 @@ import userPhoto from '../../assets/images/ava.jpg';
 class Users extends React.Component {
 
    componentDidMount(){
-    Axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+    Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+});
+   }
+   onPageChanged = (pageNumber)=>{
+    this.props.setCurrentPage(pageNumber);
+    Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
         this.props.setUsers(response.data.items);
 });
    }
     render (){
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i=1; i<=pagesCount; i++){
+            pages.push(i);
+        }
+
         return <div>
+            <div>
+                {pages.map( p=>{
+                    return <span className={this.props.currentPage === p && styles.selectedPage}
+                    onClick = {(e)=>this.onPageChanged(p)}>{p}</span>
+                })}
+            </div>
        {this.props.users.map(u=> <div key={u.id}>
         <span>
             <div>
-                <img src={u.photos.small !=null?u.photos.small :userPhoto } className={styles.userPhoto} />
+                <img src={u.photos.small !=null ? u.photos.small :userPhoto } className={styles.userPhoto} />
             </div>
             <div>
                 {u.folowwed ?
--- src/component/Users/UsersContainer.jsx
+++ src/component/Users/UsersContainer.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../state/dialogsPageReducer";
 import {connect} from "react-redux";
 import Users from "./Users";
-import {folowAC, setUsersAC, unfolowAC} from "../../state/usersPageReducer";
+import {folowAC, setUsersAC, unfolowAC, setCurrentPageAC, setTotalUsersCountAC} from "../../state/usersPageReducer";
 
 
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
     }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -20,6 +23,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) =>{
             dispatch(setUsersAC(users));
+        },
+        setCurrentPage: (pageNumber) =>{
+            dispatch(setCurrentPageAC(pageNumber));
+        },
+        setTotalUsersCount: (totalCount) =>{
+            dispatch(setTotalUsersCountAC(totalCount));
         }
     }
 }
