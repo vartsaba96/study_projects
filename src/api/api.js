38fd69424fe5b2e224b4467067388e185d370289
1Varts <mcvarc@gmail.com>
2019-06-04 15:33:09 +0300

    Refactoring usersAPI,DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as Axios from 'axios';
+
+const instance = Axios.create({
+    withCredentials: true,
+    baseURL : 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "api-key":"12617c6a-70a4-4dea-8437-dd1cdd9881f3",
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10 ){
+
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,
+         {
+        }).then(response => response.data);
+    
+    }
+}
