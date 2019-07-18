cc4cf4861b8fae477bc3428a8085f882d1281e04
1Varts <mcvarc@gmail.com>
2019-07-18 20:10:34 +0300

    Update react-create-app

--- src/component/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/component/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import s from './ProfileInfo.module.css';
-
 
 
 class ProfileStatus extends React.Component {
@@ -8,7 +6,8 @@ class ProfileStatus extends React.Component {
         editMode: false,
     }
     
-    activateEditMode (){
+    activateEditMode =() => {
+        console.log("this:", this);
         this.setState({
             editMode: true, 
         })
@@ -25,7 +24,7 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode&&
                   <div>
-                     <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                     <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                   </div>
                 }
                 {this.state.editMode&&
