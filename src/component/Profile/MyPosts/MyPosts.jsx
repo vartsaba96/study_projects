fe7f6d5ed3477888cf420ea8e82989232e524843
vartsaba96 <mcvarc@gmail.com>
2019-08-25 17:52:53 +0300

    Add shouldComponentUpdate method to MyPosts component

--- src/component/Profile/MyPosts/MyPosts.jsx
+++ src/component/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import {reduxForm,Field} from 'redux-form';
@@ -6,13 +6,22 @@ import {required,maxLengthCreator} from './../../../utils/validators';
 import {Textarea} from './../../common/formsControls/FormsControls';
 
 const maxLength10 = maxLengthCreator(10);
-const MyPosts = (props) => {
-    let postsComponent = props.posts.map(po => <Post count={po.count} message={po.message}/>);
+
+
+
+class MyPosts extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps !== this.props || nextState !== this.state;
+
+    }
+    render(){
+        console.log("Render")
+    let postsComponent = this.props.posts.map(po => <Post count={po.count} message={po.message}/>);
     let newPost = React.createRef();
 
     let addNewPost = (values)=>{
-        props.addPost(values.newPost);
-    }
+        this.props.addPost(values.newPost);
+}
     return (
         <div className={s.postsBlock}>
              <h1>My posts</h1>
@@ -23,7 +32,7 @@ const MyPosts = (props) => {
         </div>
     )
 }
-
+}
 const AddNewPost = (props) =>{
     return (
         <form onSubmit={props.handleSubmit}>
@@ -37,9 +46,9 @@ const AddNewPost = (props) =>{
         </form>
     )
 }
-
 const AddNewPostRedux = reduxForm({
     form: "newPostForm"
 })(AddNewPost);
+
 
 export default MyPosts;
